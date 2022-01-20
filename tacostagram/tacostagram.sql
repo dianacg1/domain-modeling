247e90982225993039ee356c62d372434e8b1d3c
dianacg1 <diana.carreno@kellogg.northwestern.edu>
2022-01-20 03:24:09 +0000

    proposed model for tacos

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,5 @@
-
+-- user: id, username, real name, location
+-- posts: id, posts_ids, user_id, post_time
+-- likes: id, user_id, post_id
+-- comments: id, user_id, post_id
+-- followers: id, user_id, followers_id, followed_id
