371732173cf341a4a4b28c38af9c7a1374b45c52
dianacg1 <diana.carreno@kellogg.northwestern.edu>
2022-01-20 02:51:44 +0000

    crm exercise

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,50 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS sales_person;
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  title TEXT,
+  email TEXT,
+  address TEXT,
+  phone_number INTEGER
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  sales_person_id INTEGER,
+  contact_id INTEGER,
+  activity_name TEXT,
+  activity_type TEXT,
+  activity_notes TEXT,
+  activity_date TEXT,
+  activity_time TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_name TEXT,
+  industry_id INTEGER,
+  company_id INTEGER
+);
+
+CREATE TABLE industries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    industry_name TEXT
+    );
+
+CREATE TABLE sales_person (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    company_id INTEGER
+);
